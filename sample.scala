2fa7046f01176bf606f38f18b2d8772201dd3b47
Sota Naoi <sota.naoi@juge6.jp>
2022-12-03 02:34:03 +0000

    add

--- sample.scala
+++ sample.scala
@@ -1,1 +1,27 @@
-@main def f(args: String *) = println(args.mkString)
+@main def entryPoint(args: String*): Unit = process(args.mkString)
+val support_os_list: List[String | (String | Nothing)] =
+  ("Linux", "macOS").toList
+
+def process(args: String*): Unit = {
+  if(isSupportedEnv()){
+    println("Supported!!!!")
+  }
+}
+
+def isSupportedEnv(): Boolean = {
+
+  // Initialize Flg
+  var is_supported = false
+  // Check OS Name
+  val env_os = getOperatingSystem()
+
+  if (support_os_list.contains(env_os)) {
+    is_supported = true
+  }
+  is_supported
+}
+
+def getOperatingSystem(): String = {
+  val os: String = System.getProperty("os.name")
+  os
+}
